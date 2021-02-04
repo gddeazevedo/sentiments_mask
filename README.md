dba1f9f60afb0a99671604a2a8aca9c164a5917b
gddeazevedo <gabrielcapuerj@gmail.com>
2021-02-04 19:34:37 -0300

    deleting useless folders and files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Máscara de sentimentos
-## Link that has how to get data from polyglot: https://gist.github.com/vipmax/27800a7beca430adf37fef49b292a07a
--- install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install python3-pip
-pip install pylint
-pip install pyfirmata
-sudo apt-get install -y libicu-dev
-sudo apt install python3-numpy
-sudo pip install -U git+https://github.com/aboSamoor/polyglot.git@master
-polyglot download LANG:pt
